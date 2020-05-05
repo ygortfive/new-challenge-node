daf994ce652113395635a86c6d4d524d5591cb8d
Ygor Mattos <ygormattos.b@gmail.com>
2020-05-05 20:29:07 -0300

    Create migrations and configure models

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,10 +1,29 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+} from 'typeorm';
+
+import Transaction from './Transaction';
+
+@Entity('category')
 class Category {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @ManyToOne(() => Transaction)
+  transaction: Transaction;
+
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
